7f43a65d9a29ac3ed5ffdf9db861fbb12d8447a9
Muhammad Arslan <being.arslan@outlook.com>
2024-02-01 13:09:28 +0500

    complete with older version

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,37 @@ extern crate piston_window;
 
 mod draw; //it will tell compiler to look for draw.rs file in src folder the new namespace draw is created
 mod snake;
-fn main(){
-    println!("Snake Game");
+mod game;
+
+use piston_window::*;
+use piston_window::types::Color;
+
+use crate::game::Game;
+use crate::draw::to_coord_u32;
+
+
+const BACK_COLOR: Color = [0.56, 0.8, 0.9, 1.0 ]; //RGB (142, 202, 230) sky blue color
+fn main() {
+    let (width, height) = (30, 30);
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
 }
